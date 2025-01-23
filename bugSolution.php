284b0de65d363210911778de7de797c4d557d1c8
BugHunter-X <askzenai@gmail.com>
2025-01-23 23:54:06 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,16 @@
+function foo($a, $b) {
+  if ($a === $b) {
+    return true; 
+  }
+  return false; 
+}
+
+function bar($a, $b) {
+  if ($a === $b) {
+    return true; 
+  }
+  return false; 
+}
+
+var_dump(foo(0, '0')); // bool(false)
+var_dump(bar(0, '0')); // bool(false)
